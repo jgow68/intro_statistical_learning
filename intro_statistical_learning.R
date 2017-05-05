e4e65cae49409fe7343b12defc8dc6649af72184
YJ <yongjing.yap@emas.com>
2017-05-05 17:01:21 +0800

    initiate

---
+++ intro_statistical_learning.R
@@ -0,0 +1,10 @@
+# based on intro to statistical learning with applications in R
+# Gareth James, Daniela Witten, Trevor Hastie, Robert Tibshirani
+
+
+install.packages("ISLR")
+install.packages("MASS")
+
+library(ISLR)
+library(MASS)
+
